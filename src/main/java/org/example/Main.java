a7e251c44e5f2f943ed85d5d5c52e56312ecbb61
Anna <anna@gmail.com>
2026-05-03 21:17:02 +0300

    add Demo Main

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,17 +1,14 @@
 package org.example;
-
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+import java.util.List;
+import java.util.Optional;
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
-
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
-        }
+        MovieService movieService = new MovieService();
+        TicketBookingService ticketBookingService = new TicketBookingService();
+        Customer customer = new Customer("c1", "Anna", "+380999994343");
+        Movie movie = new Movie("m1", "Terminator");
+        ticketBookingService.activateBooking("m1");
+        List<TicketBooking> list = ticketBookingService.findActiveBooking();
+        System.out.println(list);
     }
 }
